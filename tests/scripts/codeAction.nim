57898adac1b1359e18e058194c46285a13fd4724
Jake Leahy <jake@leahy.dev>
2024-12-07 17:07:46 +1100

    Remove module tests
    Require a more advanced system

--- tests/scripts/codeAction.nim
+++ tests/scripts/codeAction.nim
@@ -6,18 +6,4 @@ proc bamboo() = discard
 bambo() #[
   ^ :CodeAction ]#
 
-
-# Imports
-import strutils #[
-        ^ :CodeAction ]#
-import std/[parseopt, strformat, macrocache] #[
-             ^ :CodeAction ]#
-import std/[parseopt, strformat, macrocache] #[
-                        ^ :CodeAction ]#
-import std/[parseopt, strformat, macrocache] #[
-                                    ^ :CodeAction ]#
-import strutils, macros #[
-          ^ :CodeAction ]#
-
-
 #> :wait TextChanged
